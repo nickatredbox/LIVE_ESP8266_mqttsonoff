f63735ea261e478e11f1260583e777f6979c03f4
Nick Goodey <nickatredbox@gmail.com>
2020-04-27 17:17:25 +1200

    Update LIVE_ESP8266_mqttsonoff.ino

--- LIVE_ESP8266_mqttsonoff.ino
+++ LIVE_ESP8266_mqttsonoff.ino
@@ -1,38 +1,43 @@
 
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 #include <MQTT.h>
 #include <Arduino_JSON.h>
 #include <EEPROM.h>
 
 
-#define REALY_PIN 12
+#define RELAY_PIN 12
 #define LED_PIN 13 //LED_BUILTIN is built in LED
 
 const char ssid[] = "Cosmo";
 const char pass[] = "Fairline19!";
+String device = "mqttsonoff";
 
 WiFiClient net;
 MQTTClient MQTTclient;
+
 
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 
 int RelayState  = LOW;
 
-uint tick = 0;
-
-
+unsigned long tick = 0;
+unsigned long UpdateCount = 6;
+unsigned long CountDownSeconds = 0;
+unsigned long CountDown = 0;
 uint addr = 0;
 
 // EEPROM data
 struct { 
   int RelayState;
+  unsigned long UpdateCount;
 } SavedData;
 
 
 void setup() {
-  
-  pinMode(REALY_PIN, OUTPUT);
+
+  pinMode(RELAY_PIN, OUTPUT);
   
   pinMode(LED_PIN, OUTPUT);
   
@@ -60,51 +65,13 @@ void setup() {
 
   if(SavedData.RelayState == HIGH)
   {        
-    RelayState  = HIGH;
-    digitalWrite(REALY_PIN, RelayState);
-    MQTTclient.publish("/mqttsonoff/status", "on");
+    control("ON");
   }
   else if(SavedData.RelayState == LOW)
   {
-    RelayState  = LOW;
-    digitalWrite(REALY_PIN, RelayState);
-    MQTTclient.publish("/mqttsonoff/status", "off");
+    control("OFF");
   }
 
-}
-
-
-void loop() {
-  
-  MQTTclient.loop();
-
-  delay(10);  // <- fixes some issues with WiFi stability
-
-  if (!MQTTclient.connected()) {
-    WiFiConnect();
-  }
-
-  if( ((tick++) % 10)== 0 )
-  {
-     
-      digitalWrite(LED_PIN, LOW);
-  
-      int rssi = WiFi.RSSI();
-      
-      JSONVar myArray;
-      
-      myArray[0] = RelayState;
-      myArray[1] = rssi;
-  
-      String jsonString = JSON.stringify(myArray);
-      
-      MQTTclient.publish("/mqttsonoff/sensors", jsonString );
-      Serial.println("/mqttsonoff/sensors" + jsonString );
-  
-      digitalWrite(LED_PIN, HIGH);
-  }
-
-  delay(100); 
 }
 
 
@@ -135,34 +102,131 @@ void WiFiConnect() {
 
   Serial.println("\nMQTT Connected!");
 
-  MQTTclient.subscribe("/mqttsonoff/control");  
+  MQTTclient.subscribe("/" + device + "/control");  
 
-  MQTTclient.publish("/mqttsonoff/status", "start");
-  MQTTclient.publish("/mqttsonoff/localip", "/mqttsonoff/localip/" + WiFi.localIP().toString());
+  MQTTclient.publish("/" + device + "/status", "start");
+  MQTTclient.publish("/" + device + "/localip", "/" + device + "/localip/" + WiFi.localIP().toString());
   
   digitalWrite(LED_PIN,HIGH);  
 }
 
-void messageReceived(String &topic, String &payload) {
+
+void loop() {
+  
+  MQTTclient.loop();
+
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!MQTTclient.connected()) {
+    WiFiConnect();
+  }
+
+  if( (tick % (UpdateCount * 10))== 0 )
+  {
+     SendData();
+  }
+
+  if( (CountDownSeconds > 0) && ( (tick % 10) == 0) )
+  {    
+    if( CountDown == 0 )
+    {
+      CountDownSeconds = 0;
+      control("OFF");  
+    }
+    else
+      CountDown--;
+         
+  }
+  
+  tick++;
+
+  delay(90); 
+}
+
+void SendData()
+{
+    digitalWrite(LED_PIN, LOW);
+
+    int rssi = WiFi.RSSI();
+    
+    JSONVar myArray;
+    
+    myArray[0] = RelayState;
+    myArray[1] = rssi;
+    myArray[2] = CountDown;
+    myArray[3] = tick;
+    myArray[4] = UpdateCount;
+
+    String jsonString = JSON.stringify(myArray);
+    
+    MQTTclient.publish("/" + device + "/sensors", jsonString );
+    Serial.println("/" + device + "/sensors" + jsonString );
+
+    digitalWrite(LED_PIN, HIGH);
+}
+
+
+void control(String payload) 
+{
+  String topic = "/" + device + "/control";
+  DeviceControl(topic, payload);
+}
+
+void  messageReceived(String &topic, String &payload) {
+
+  DeviceControl(topic, payload);
+}
+  
+void DeviceControl(String topic, String payload) {
   
   Serial.println("incoming: topic:" + topic + "  Payload:" + payload);
 
   digitalWrite(LED_PIN, LOW);
 
-  if(topic.startsWith("/mqttsonoff/control"))
-  {
-    if(payload.startsWith("read"))
+  if(topic.startsWith("/" + device + "/control"))
+  {    
+    if(payload.startsWith("CountDown"))
     {
-      MQTTclient.publish("/mqttsonoff/localip", "/mqttsonoff/localip/" + WiFi.localIP().toString());
+      CountDownSeconds = (long)payload.substring(10).toInt();
+     
+      CountDown = CountDownSeconds;
+
+      if(CountDownSeconds > 0)
+      {
+        RelayState  = HIGH;
+        digitalWrite(RELAY_PIN, RelayState);
+        MQTTclient.publish("/" + device + "/status", "on");        
+      }
+      else
+      {
+        RelayState  = LOW;
+        digitalWrite(RELAY_PIN, RelayState);
+        MQTTclient.publish("/" + device + "/status", "off");       
+      }
+      
+      SendData();
+    }
+    else if(payload.startsWith("UpdateCount"))
+    {
+      UpdateCount =(long)payload.substring(12).toInt();
+      if(UpdateCount > 120) UpdateCount = 120;
+      if(UpdateCount < 1) UpdateCount = 1;
+      SaveEEPROM();
+    }
+    else if(payload.startsWith("read"))
+    {
+      MQTTclient.publish("/" + device + "/localip", "/" + device + "/localip/" + WiFi.localIP().toString());
       
       if(RelayState == HIGH)
       {        
-        MQTTclient.publish("/mqttsonoff/status", "on");
+        MQTTclient.publish("/" + device + "/status", "on");
       }
       else if(RelayState == LOW)
       {
-        MQTTclient.publish("/mqttsonoff/status", "off");
+        MQTTclient.publish("/" + device + "/status", "off");
       }
+      
+      SendData();
     }
     else if(topic.indexOf("control") > 0)
     {         
@@ -175,44 +239,58 @@ void messageReceived(String &topic, String &payload) {
             
           if(RelayState == HIGH)
           {        
-            MQTTclient.publish("/mqttsonoff/status", "on");
+            MQTTclient.publish("/" + device + "/status", "on");
           }
           else if(RelayState == LOW)
           {
-            MQTTclient.publish("/mqttsonoff/status", "off");
+            MQTTclient.publish("/" + device + "/status", "off");
           }
 
-          digitalWrite(REALY_PIN, RelayState);
+          digitalWrite(RELAY_PIN, RelayState);
+         
         }
         else if(payload.startsWith("ON"))
         {        
+          CountDown = 0;
+          CountDownSeconds = 0;
           RelayState  = HIGH;
-          digitalWrite(REALY_PIN, RelayState);
-          MQTTclient.publish("/mqttsonoff/status", "on");
+          digitalWrite(RELAY_PIN, RelayState);
+          MQTTclient.publish("/" + device + "/status", "on");        
         }
         else if(payload.startsWith("OFF"))
         {
+          CountDown = 0;
+          CountDownSeconds = 0;
           RelayState  = LOW;
-          digitalWrite(REALY_PIN, RelayState);
-          MQTTclient.publish("/mqttsonoff/status", "off");
+          digitalWrite(RELAY_PIN, RelayState);
+          MQTTclient.publish("/" + device + "/status", "off");         
         }
-
-         SavedData.RelayState = RelayState;
-
-        // replace values in byte-array cache with modified data
-        // no changes made to flash, all in local byte-array cache
-        EEPROM.put(addr,SavedData);
         
-        // actually write the content of byte-array cache to
-        // hardware flash.  flash write occurs if and only if one or more byte
-        // in byte-array cache has been changed, but if so, ALL 512 bytes are 
-        // written to flash
-        EEPROM.commit();  
+        SendData();
+        
+        SaveEEPROM();
        
     }
   
-    MQTTclient.publish("/mqttsonoff/status", topic + "/" + payload);
+    MQTTclient.publish("/" + device + "/status", topic + "/" + payload);
     
     digitalWrite(LED_PIN, HIGH);
   }
+}
+
+void SaveEEPROM()
+{
+ 
+    SavedData.UpdateCount = UpdateCount;
+    SavedData.RelayState = RelayState;
+
+    // replace values in byte-array cache with modified data
+    // no changes made to flash, all in local byte-array cache
+    EEPROM.put(addr,SavedData);
+    
+    // actually write the content of byte-array cache to
+    // hardware flash.  flash write occurs if and only if one or more byte
+    // in byte-array cache has been changed, but if so, ALL 512 bytes are 
+    // written to flash
+    EEPROM.commit();  
 }
